ea6bdc1f2e25dfcf9f58f99b434a9da5cdc3caf6
Luís Filipe Durães Melo <luis.f.duraes.melo@gmail.com>
2022-09-08 22:07:36 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ dotenv.config();
 const secret = process.env.SESSION_SECRET;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
