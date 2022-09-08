356ddb1e799ab1c1ab874379b2394f06a8ef6bfd
Luís Filipe Durães Melo <luis.f.duraes.melo@gmail.com>
2022-09-08 22:13:35 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,13 @@ async function bootstrap() {
       'authorization',
     ],
   });
+  app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
   app.use(cookieParser());
   app.use(helmet());
   app.useGlobalPipes(
