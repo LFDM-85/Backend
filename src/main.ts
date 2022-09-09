5f5238944183aba6f1fa4efa2432ffa60ef08a7f
Luís Filipe Durães Melo <luis.f.duraes.melo@gmail.com>
2022-09-09 19:06:22 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -15,19 +15,20 @@ const secret = process.env.SESSION_SECRET;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-    preflightContinue: true,
-    allowedHeaders: [
-      'origin',
-      'x-requested-with',
-      'content-type',
-      'accept',
-      'authorization',
-    ],
-  });
+  app.enableCors();
+  // app.enableCors({
+  //   origin: '*',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   credentials: true,
+  //   preflightContinue: true,
+  //   allowedHeaders: [
+  //     'origin',
+  //     'x-requested-with',
+  //     'content-type',
+  //     'accept',
+  //     'authorization',
+  //   ],
+  // });
 
   app.use(cookieParser());
   app.use(helmet());
