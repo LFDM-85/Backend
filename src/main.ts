28f0ed6210edf45aa15a99228845304fdb8510bd
Luís Filipe Durães Melo <luis.f.duraes.melo@gmail.com>
2022-09-09 15:23:46 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import * as cookieParser from 'cookie-parser';
 import * as session from 'express-session';
 import * as passport from 'passport';
 import * as dotenv from 'dotenv';
-import cors from 'cors'
 
 dotenv.config();
 
@@ -15,33 +14,18 @@ const secret = process.env.SESSION_SECRET;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const whitelist = ['http://frontendtest-livid.vercel.app'];
-  app.use(cors());
-  app.use(function (req, res, next) {
-    let i = 0,
-      notfound = 1,
-      const referer = req.get('Referer');
-    if (referer) {
-      while (i < global.whitelist.length && notfound) {
-        notfound = referer.indexOf(whitelist[i]) === -1;
-        i++;
-      }
-    }
-    if (notfound) res.send('Denied!');
-    else next();
+  app.enableCors({
+    origin: 'http://frontendtest-livid.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: [
+      'origin',
+      'x-requested-with',
+      'content-type',
+      'accept',
+      'authorization',
+    ],
   });
-  // app.enableCors({
-  //   origin: 'http://frontendtest-livid.vercel.app',
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  //   credentials: true,
-  //   allowedHeaders: [
-  //     'origin',
-  //     'x-requested-with',
-  //     'content-type',
-  //     'accept',
-  //     'authorization',
-  //   ],
-  // });
 
   app.use(cookieParser());
   app.use(helmet());
@@ -62,5 +46,3 @@ async function bootstrap() {
   await app.listen(5000);
 }
 bootstrap();
-
-
