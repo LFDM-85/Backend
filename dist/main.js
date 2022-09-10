9f0adee210f05375aea7fc34b132344826afe5d9
Luís Filipe Durães Melo <luis.f.duraes.melo@gmail.com>
2022-09-10 15:09:11 +0100

    main update

--- dist/main.js
+++ dist/main.js
@@ -11,25 +11,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const secret = process.env.SESSION_SECRET;
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    const allowCors = (fn) => async (req, res) => {
-        res.setHeader('Access-Control-Allow-Credentials', true);
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-        res.setHeader('HTTP/1.1 200 OK');
-        res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-        if (req.method === 'OPTIONS') {
-            res.status(200).end();
-            return;
-        }
-        return await fn(req, res);
-    };
-    const handler = (req, res) => {
-        const d = new Date();
-        res.end(d.toString());
-    };
-    module.exports = allowCors(handler);
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true });
     app.use(cookieParser());
     app.use((0, helmet_1.default)());
     app.useGlobalPipes(new common_1.ValidationPipe({
