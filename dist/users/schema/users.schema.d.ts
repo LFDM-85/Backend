409f164e7f451c963fff14474d31ce7998002f38
Luís Filipe Durães Melo <luis.f.duraes.melo@gmail.com>
2022-09-10 14:27:13 +0100

    dist folder deleted

--- dist/users/schema/users.schema.d.ts
+++
@@ -1,9 +0,0 @@
-import * as mongoose from 'mongoose';
-export declare const UsersSchema: mongoose.Schema<any, mongoose.Model<any, any, any, any, any>, {}, {}, {}, {}, "type", {
-    email: string;
-    password: string;
-    name: string;
-    role: any[];
-    refreshToken?: string;
-    refreshTokenExp?: string;
-}>;
