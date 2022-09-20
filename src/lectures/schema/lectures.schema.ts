373cc37f1a20ed056ae6509cdbc9b3991b1ed7f6
Luís Filipe Durães Melo <luis.f.duraes.melo@gmail.com>
2022-09-20 11:45:53 +0100

    Update lectures.schema.ts

--- src/lectures/schema/lectures.schema.ts
+++ src/lectures/schema/lectures.schema.ts
@@ -1,7 +1,5 @@
 import { Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
 import { Transform } from "class-transformer";
-import mongoose, { Document } from "mongoose";
-import { Class } from "src/class/schema/class.schema";
 
 export type LectureDocument = Lecture & Document
 @Schema()
@@ -14,8 +12,7 @@ export class Lecture {
   description: string;
   @Prop()
   finished: boolean;
-   @Prop(({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: Class.name }] }))
-  class: Class
+  
 }
 
-export const LectureSchema = SchemaFactory.createForClass(Lecture)
+export const LectureSchema = SchemaFactory.createForClass(Lecture);
