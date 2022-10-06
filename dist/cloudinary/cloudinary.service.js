ca0b641c8555bfad0c1b560b540b2c6c4c08c2c0
Luís Melo <luis.f.duraes.melo@gmail.com>
2022-10-06 19:02:57 +0100

    update

--- dist/cloudinary/cloudinary.service.js
+++ dist/cloudinary/cloudinary.service.js
@@ -13,11 +13,7 @@ const buffer_to_stream_1 = require("buffer-to-stream");
 let CloudinaryService = class CloudinaryService {
     async uploadfile(file) {
         return new Promise((resolve, reject) => {
-            const upload = cloudinary_1.v2.uploader.upload_stream({ upload_preset: 'ml_default' }, (error, result) => {
-                if (error)
-                    return reject(error);
-                resolve(result);
-            });
+            const upload = cloudinary_1.v2.uploader.upload_stream({ upload_preset: 'ml_default' });
             (0, buffer_to_stream_1.default)(file.buffer).pipe(upload);
         });
     }
