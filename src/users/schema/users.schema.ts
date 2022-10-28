b14b3c2172aee5d801c4cf636448e627521c34e3
Luís Melo <luis.f.duraes.melo@gmail.com>
2022-10-28 11:22:08 +0100

    update prettier configuration

--- src/users/schema/users.schema.ts
+++ src/users/schema/users.schema.ts
@@ -26,13 +26,6 @@ export class Users {
   // @Prop([{type: mongoose.Schema.Types.ObjectId, ref: Class.name}])
   @Prop({ type:[{ type: mongoose.Schema.Types.ObjectId, ref: Class.name}]})
   classes: [Class]
-  // @Prop({type: [{ type: mongoose.Schema.Types.ObjectId, ref: Work.name }]})
-  // work: Work[];
-  //   @Prop({type: [{ type: mongoose.Schema.Types.ObjectId, ref: Assessment.name }]})
-  //   assessment: Assessment[];
-  //   @Prop({type: [{ type: mongoose.Schema.Types.ObjectId, ref: Attendance.name }]})
-  // attendance: Attendance[];
-
 }
 
 export const UsersSchema = SchemaFactory.createForClass(Users)
