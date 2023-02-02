fe950f8a2ae779713a0a5fb8c3fad1409a52cd25
Luís Melo <luis.f.duraes.melo@gmail.com>
2023-02-02 11:02:54 +0000

    test

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import helmet from 'helmet'
 import * as dotenv from 'dotenv'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
-dotenv.config()
+dotenv.config({ path: './var/.env' })
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
